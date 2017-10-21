375e53e39d5018a85d5d96c3a4b91e87a64a7bb9
Melvin Vivas <melvindave@gmail.com>
2017-10-21 18:08:22 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # web3js-samples
 NodeJS sample application calling Ethereum API
+
+## Install all dependencies using npm
+
+```bash
+npm install
+```
+
+## Configure
+
+Create a .env file with the ff. contents
+
+```
+API_URL=http://.../<access key>
+ACCOUNT=
+```
+
+## Run the application
+
+```bash
+node index.js
+```
+
+
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+var BigNumber = require('bignumber.js');
+var Web3 = require('web3')
+
+var web3 = new Web3(new Web3.providers.HttpProvider(process.env.API_URL));
+
+var balance = web3.eth.getBalance(process.env.ACCOUNT, function (error, result) {
+    if (!error) {
+	console.log(web3.utils.fromWei(new BigNumber(result), 'ether'));
+    } else {
+      console.error(error);
+    }
+  });
+
+
